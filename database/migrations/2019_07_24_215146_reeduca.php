aa2d3bc79a1917e307c356f8fd60f97180111888
bbuenodeveloper <b.buenoo94@gmail.com>
2019-07-24 20:10:36 -0300

    Iniciando o projeto integrador com laravel, mais pagina home, sobre, cotato, login e regitro. Mais banco de dados reeduca com newsletter.

---
+++ database/migrations/2019_07_24_215146_reeduca.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Reeduca extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('newsletter', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('email')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
