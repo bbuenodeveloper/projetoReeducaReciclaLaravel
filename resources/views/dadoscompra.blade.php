d4432411c10cf6f526793674797d2733831764a2
Thiagofrf <thiago.fontanella@hotmail.com>
2019-08-07 23:12:35 -0300

    resolvendo problema no dados compra e corrigindo nivel de usuario do seeder

--- resources/views/dadoscompra.blade.php
+++ resources/views/dadoscompra.blade.php
@@ -97,17 +97,7 @@
                 </div>
                 @endif
                 @else
-                <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link efeitoTransicao dropdown-toggle" href="#" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }} <span class="caret"></span>
-                    </a>
-
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                            {{ __('Sair') }}
-                        </a>
+                
 
 
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
