37e8adf49d5ae92681836e6aa886816121e72e96
marfrancis <marcelomediasolution@gmail.com>
2019-08-09 10:50:17 -0300

    melhoria no drop down do login home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -95,7 +95,7 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     @if (Auth::user()->nivel_user === 0)
-                                    <a class="dropdown-item" href="{{ route('painel') }}"><i class="fa fa-cog"></i><span class="space-menu"> {{ __('Painel') }} </span></a>
+                                    <a class="dropdown-item" href="{{ route('painel') }}"><i class="fa fa-cog"></i><span class="space-menu"> {{ __('Painel Admin') }} </span></a>
                                     @endif
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
