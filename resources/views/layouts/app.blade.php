67461619cdb231aaf4158afde5e18748ed62ab46
Ana Baccarin <anamabaccarin@gmail.com>
2019-08-15 17:52:29 -0300

    Register blade, app blade, rota

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,7 +91,7 @@
                                 @else
                                 <li class="nav-item dropdown">
                                     <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre><i class="fas fa-user text-white p-2"></i>
-                                        {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{Auth::user()->name}}<span class="caret"></span>
                                     </a>
 
                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
