7f2e1506a2f174baaa3d444d01a651650044fec3
Ana Baccarin <anamabaccarin@gmail.com>
2019-08-19 10:56:59 -0300

    Config inicial para socialite facebook

--- config/services.php
+++ config/services.php
@@ -44,4 +44,11 @@ return [
         ],
     ],
 
+    'facebook' => [
+        'client_id' => env('FACEBOOK_ID'),
+        'client_secret' => env('FACEBOOK_SECRET'),
+        'redirect' => env('FACEBOOK_REDIRECT')
+        
+    ]
+
 ];
