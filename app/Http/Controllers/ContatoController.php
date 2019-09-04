64629ab8255d47fd408f2a46d0edf6895dec5e48
marfrancis <marcelomediasolution@gmail.com>
2019-09-04 17:02:57 -0300

    comflito imagem

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -15,16 +15,8 @@ class ContatoController extends Controller
 
     function sendMail(Request $request) {
         try {
-<<<<<<< HEAD
-            $objMailTo = Mail::to('contato@reciclamaps.com.br');
-            $mailable = new MsgContactMailable($name, $email, $message);
-            $objMailTo->send($mailable);
-            $msgRetorno ="Email enviado com sucesso, obrigado!";
-
-=======
             Mail::to(config('mail.to.address'))->send(new FormularioContato($request));
             return view('contato', ['msg' => 'Mensagem enviada com sucesso!']);
->>>>>>> e0060e409d762f00e59db548a79e2b23cb241e7c
         } catch (\Exception $e) {
             //echo $e->getMessage();
             $msgRetorno = "Ocorreu um erro ao tentar enviar sua mensagem. Por favor, tente novamente mais tarde";
